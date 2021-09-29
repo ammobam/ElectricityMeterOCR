3a266f64ef7546a38a8ec724e5336e950a66c6a8
yujapie <yujapie@gmail.com>
2021-09-29 16:03:34 +0900

    전처리, 상세조회 화면 추가

---
+++ app/src/main/java/com/lfin/electricitymeterocr/MeterInfoDetailActivity.java
@@ -0,0 +1,14 @@
+package com.lfin.electricitymeterocr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeterInfoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meter_info_detail);
+    }
+}
---
+++ app/src/main/java/com/lfin/electricitymeterocr/PreprocessingImageActivity.java
@@ -0,0 +1,14 @@
+package com.lfin.electricitymeterocr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreprocessingImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preprocessing_image);
+    }
+}
